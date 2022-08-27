f9b6e53d86ebfa84ea8fd360cd52c5dc810c4a1a
AKASH KUMAR <akash30521@gmail.com>
2022-08-27 18:02:14 +0530

    Create solution_2.cpp

---
+++ Largest Rectangle in Histogram/solution_2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n= heights.size();
+        vector<int> leftSmaller(n,0);
+        vector<int> rightSmaller(n,n-1);
+        stack<int> s;
+        for(int i=0;i<n;i++){
+            while(!s.empty() && heights[s.top()] >= heights[i])
+                s.pop();
+            if(s.empty())
+                leftSmaller[i] = 0;
+            else
+                leftSmaller[i] = s.top()+1;
+            s.push(i);
+        }
+        while(!s.empty())
+            s.pop();
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && heights[s.top()] >= heights[i])
+                s.pop();
+            if(s.empty())
+                rightSmaller[i] = n-1;
+            else
+                rightSmaller[i] = s.top()-1;
+            s.push(i);
+        }
+        int maxArea = 0;
+        for(int i=0;i<n;i++){
+            maxArea = max(maxArea, (rightSmaller[i]-leftSmaller[i]+1) * heights[i]);
+        }
+        return maxArea;
+    }
+};
